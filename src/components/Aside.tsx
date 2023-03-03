9086a4ee596698cbabc09ab08f749ebf6083af77
Camilo Nore <caminore22@gmail.com>
2023-03-03 13:02:59 -0500

    Aside Form and Styles

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,7 +1,50 @@
 import styles from '../styles/Aside.module.css'
+import common from '../styles/common.module.css'
 
 function Aside() {
-  return <aside className={styles.aside}>Aside</aside>
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const height = e.currentTarget.height.value
+    const width = e.currentTarget.width.value
+    console.log(height, width)
+  }
+  const formFields = [
+    {
+      label: 'Height:',
+      name: 'height',
+      id: 'height',
+      type: 'text'
+    },
+    {
+      label: 'Width:',
+      name: 'width',
+      id: 'width',
+      type: 'text'
+    }
+  ]
+
+  return (
+    <aside className={styles.aside}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        {formFields.map((field) => (
+          <section className={styles.section}>
+            <label className={common.label} htmlFor={field.id}>
+              {field.label}
+            </label>
+            <input
+              className={common.input}
+              type={field.type}
+              name={field.name}
+              id={field.id}
+            />
+          </section>
+        ))}
+        <button className={common.button} type='submit'>
+          Apply!
+        </button>
+      </form>
+    </aside>
+  )
 }
 
 export { Aside }
