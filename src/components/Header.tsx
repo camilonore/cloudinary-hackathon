882563b2a812f10fb98c1f92936f71aa8bbcc721
Camilo Nore <caminore22@gmail.com>
2023-03-03 13:03:17 -0500

    Header styles and export button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Header.module.css'
+import common from '../styles/common.module.css'
 function Header() {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      <h1>CloudVideoEdit</h1>
+      <button className={common.button}>Export!</button>
       <div className={styles.roundedPrev}>
         <div className={styles.circle} />
       </div>
