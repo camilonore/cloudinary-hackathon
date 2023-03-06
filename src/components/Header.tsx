912ca125509d356707916819d37f148bf66282c1
Camilo Nore <caminore22@gmail.com>
2023-03-06 18:04:31 -0500

    Download icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import styles from '../styles/Header.module.css'
 import common from '../styles/common.module.css'
+import { DownloadCloud } from './Icons/DownloadCloud'
 function Header() {
   return (
     <header className={styles.header}>
       <h1>CloudVideoEdit</h1>
-      <button className={common.button}>Export!</button>
+      <button className={common.button}>
+        Export!
+        <DownloadCloud />
+      </button>
       <div className={styles.roundedPrev}>
         <div className={styles.circle} />
       </div>
